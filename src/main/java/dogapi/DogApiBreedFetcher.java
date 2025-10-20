9899ae00912f07bb982bf713295601d7ef4c9a11
kelongrenrenren <liuyx1130@gmail.com>
2025-10-20 10:03:09 -0400

    Task 1

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -19,19 +19,48 @@ public class DogApiBreedFetcher implements BreedFetcher {
 
     /**
      * Fetch the list of sub breeds for the given breed from the dog.ceo API.
+     *
      * @param breed the breed to fetch sub breeds for
      * @return list of sub breeds for the given breed
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
     public List<String> getSubBreeds(String breed) {
-        // TODO Task 1: Complete this method based on its provided documentation
+        String url = "https://dog.ceo/api/breed/" + breed + "/list";
+        Request request = new Request.Builder().url(url).build();
+
+        try (Response response = client.newCall(request).execute()) {
+            String jsonText = response.body().toString();
+
+            JSONObject jsonObject = new JSONObject(jsonText);
+
+            String status = jsonObject.getString("status");
+            if (status.equals("error")) {
+                String message = jsonObject.getString("message");
+                throw new BreedNotFoundException(message);
+            }
+
+            JSONArray jsonArray = jsonObject.getJSONArray("message");
+            List<String> subBreeds = new ArrayList<>();
+            for (int i = 0; i < jsonArray.length(); i++) {
+                subBreeds.add(jsonArray.getString(i));
+            }
+
+            return subBreeds;
+
+        } catch (IOException e) {
+            throw new BreedNotFoundException(breed);
+        }
+    }
+}
+
+
+
+
+        // Task 1: Complete this method based on its provided documentation
         //      and the documentation for the dog.ceo API. You may find it helpful
         //      to refer to the examples of using OkHttpClient from the last lab,
         //      as well as the code for parsing JSON responses.
         // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
-    }
-}
+        // return new ArrayList<>();
 
-testing
